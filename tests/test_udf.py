f69f9edd6ef6929e58b43cff54169eaa6e9f567d
Adam Bronte <adam@bronte.me>
2020-07-29 21:03:00 -0700

    pickle any pyspark types arguments (#8)

--- tests/test_udf.py
+++ tests/test_udf.py
@@ -1,6 +1,7 @@
 import pytest
 
 import pyspark.sql.functions as F
+from pyspark.sql.types import IntegerType
 
 spark = pytest.spark
 
@@ -27,3 +28,16 @@ def test_udf_sql_function():
     rows = spark.sql('select add(id) AS id FROM table').collect()
 
     assert rows[0].id == '3'
+
+def test_udf_with_schema():
+    def sub(s):
+        return s - 1
+
+    spark.udf.register("sub", sub, IntegerType())
+
+    df = spark.createDataFrame([{'id':2},{'id': 2}], ['id'])
+    df.createOrReplaceTempView('table')
+
+    rows = spark.sql('select sub(id) AS id FROM table').collect()
+
+    assert rows[0].id == 1
